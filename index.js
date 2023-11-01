cae6ee9bf3de216ed2de3bd5c38fa91746bf2ad6
jeproch <jprevenant@gmail.com>
2023-11-01 08:53:28 -0700

    Check mobile

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 //Check if user is on mobile
 
+document.addEventListener('DOMContentLoaded', function() {
+    var isMobile =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
+    
+    if (isMobile === true) {
+        window.location.href = './mobileFail/mobile.html'
+    } else {
+      alert('This website uses cookies to store data');
+    }
+})
