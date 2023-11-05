66840e62e0a308d7212c969787076c3e1733fed4
jeproch <jprevenant@gmail.com>
2023-11-04 20:59:32 -0700

    fdfds

--- game/game.js
+++ game/game.js
@@ -1,6 +1,7 @@
 let START_BTN = document.getElementById('start-btn')
 const bodyEl = document.getElementById("body")
 
+
 document.addEventListener('DOMContentLoaded', function() {
     const mainMenu = document.getElementById('main-menu')
     mainMenu.classList.remove('hide')
@@ -22,27 +23,7 @@ START_BTN.addEventListener('click', function() {
     // Set the body background image to the image in the .game container
     document.body.style.backgroundImage = `url('${gameMenu.querySelector('.background-image').src}')`;
     bodyEl.classList.add("black-background")
+    
 })
 
-/* This is where the orcs will be loaded on the orc path, they need to run so go through and use timeout */
 
-
-/*
-
-const imageUrls = [
-    'image1.jpg',
-    'image2.jpg',
-    'image3.jpg',
-    // Add more image URLs here
-];
-
-let currentIndex = 0;
-
-function changeBackground() {
-    document.body.style.backgroundImage = `url(${imageUrls[currentIndex]})`;
-    currentIndex = (currentIndex + 1) % imageUrls.length;
-}
-
-setInterval(changeBackground, 5000); // Change every 5 seconds (5000 milliseconds)
-
-*/
--- game/orcmovement.js
+++ game/orcmovement.js
@@ -1,23 +1,47 @@
-let orcCanvas = document.querySelector('.orc canvas'); // If you want to use the class
+let orcCanvas = document.querySelector('.orc'); // If you want to use the class
 let Move_ORC_BTN = document.getElementById("move-orc-btn")
 let orcPath = document.querySelector('.orc-path canvas')
+const images = [
+    "../game/game images/Orcs/Orc 1 - Unact.png",
+    "../game/game images/Orcs/Orc 1 - Act.png"
+]
+
+document.addEventListener('DOMContentLoaded', function() {
+    //Start with unactive image 
+    orcCanvas.style.backgroundImage = 'url("../game/game images/Orcs/Orc 1 - Unact.png")';
+})
 
 orcCanvas.style.left = '85vw'
 orcPath.style.left = '30vw'
 
-Move_ORC_BTN.addEventListener('click', function() {
-    console.log(orcCanvas.style.left)
+let currentIndex = 0;
 
-    let currentLeft = parseInt(orcCanvas.style.left);
-    let CurrentPath = parseInt(orcPath.style.left)
-
-    // Adjust the left value, for example, increase it by 10vw
-    currentLeft -= 0.25;
-    
-    // Set the updated left value
-    orcCanvas.style.left = currentLeft + 'vw';
-
-    if (currentLeft <= CurrentPath) {
-        alert('oor')
+function changeImageOrc() {
+    orcCanvas.style.backgroundImage = `url(${images[currentIndex]})`
+    currentIndex = currentIndex + 1
+    if (currentIndex >= 1) {
+     currentIndex = 0;
     }
+}
+
+Move_ORC_BTN.addEventListener('click', function() {
+   let currentLeft = parseInt(orcCanvas.style.left);
+   let CurrentPath = parseInt(orcPath.style.left)
+
+   // Adjust the left value, for example, increase it by 10vw
+   currentLeft -= 0.25;
+   
+   // Set the updated left value
+   orcCanvas.style.left = currentLeft + 'vw';
+
+   if (currentLeft <= CurrentPath) {
+       alert('oor')
+   }
+
+   setInterval(changeImageOrc, 500);
+   console.log(currentIndex)
 })
+
+export function main() {
+    console.log('')
+}
