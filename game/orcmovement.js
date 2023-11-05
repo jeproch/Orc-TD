cce80b5ee8e664cae33f949a3c7bdf321a0683fa
jeproch <jprevenant@gmail.com>
2023-11-04 21:23:02 -0700

    Removed images array

--- game/orcmovement.js
+++ game/orcmovement.js
@@ -1,10 +1,6 @@
 let orcCanvas = document.querySelector('.orc'); // If you want to use the class
 let Move_ORC_BTN = document.getElementById("move-orc-btn")
 let orcPath = document.querySelector('.orc-path canvas')
-const images = [
-    "../game/game images/Orcs/Orc 1 - Unact.png",
-    "../game/game images/Orcs/Orc 1 - Act.png"
-]
 
 document.addEventListener('DOMContentLoaded', function() {
     //Start with unactive image 
