9606c9184815d2cfdf43850994ad8fab089db327
Ben <vantonderbenna@gmailcom>
2024-01-08 22:17:30 +0200

    this file works. IDFK what this does

--- game/orc_development.js
+++ game/orc_development.js
@@ -1,3 +1,11 @@
-// How does the orc become harder to kill as time progresses??
-let orcHealth = 30;
-export { orcHealth };
+// In orc_development.js
+let orcHealthAdvances; // Declare the variable outside the event listener
+let orcDamageAdvances;
+
+document.addEventListener("DOMContentLoaded", function () {
+  orcHealthAdvances = parseFloat(localStorage.getItem("waveCounterLocal")) || 0;
+  orcDamageAdvances = parseFloat(localStorage.getItem("waveCounterLocal")) || 0;
+});
+
+export { orcHealthAdvances };
+export { orcDamageAdvances };
